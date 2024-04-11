3256c170ac4b977c1313db93c514d1c101963126
KäseToatz <airfryerkipcorn@gmail.com>
2024-04-11 16:39:39 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name="Discord SDK",
+   version="1.0",
+   description="Discord Tools",
+   author="KaseToatz1337",
+   packages=["discord-sdk"],
+   install_requires=["discord.py"]
+)
